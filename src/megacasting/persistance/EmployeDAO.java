af1a406dd679f5b9abd95682bbce3d3362803006
jbureau <julien.bureau02@gmail.com>
2014-12-01 15:48:03 +0100

    [~] FIX BUG NetBeans

---
+++ src/megacasting/persistance/EmployeDAO.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package megacasting.persistance;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+import megacasting.entity.Employe;
+
+/**
+ * @author julien
+ * 01-12-2014
+ */
+public class EmployeDAO {
+
+	public void insert(Connection cnx, Employe employe) {
+		
+		
+        
+	}
+	
+}
