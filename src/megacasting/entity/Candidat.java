3ff56794d57a1d6c3fdf5980d39687c2ec4c9690
jbureau <julien.bureau02@gmail.com>
2014-12-01 14:07:30 +0100

    [+] BO Class Candidat.java

---
+++ src/megacasting/entity/Candidat.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package megacasting.entity;
+
+import java.util.Date;
+
+
+
+/**
+ * @author julien
+ * 01-12-2014
+ */
+public class Candidat {
+
+	private long identifiant;
+    private String nom;
+    private String prenom;
+    private Date dateNaissance;
+    private String pays;
+    
+    
+	/**
+	 * @return the identifiant
+	 */
+	public long getIdentifiant() {
+		return identifiant;
+	}
+	/**
+	 * @param identifiant the identifiant to set
+	 */
+	public void setIdentifiant(long identifiant) {
+		this.identifiant = identifiant;
+	}
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	/**
+	 * @return the dateNaissance
+	 */
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	/**
+	 * @param dateNaissance the dateNaissance to set
+	 */
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	/**
+	 * @return the pays
+	 */
+	public String getPays() {
+		return pays;
+	}
+	/**
+	 * @param pays the pays to set
+	 */
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+    
+}
