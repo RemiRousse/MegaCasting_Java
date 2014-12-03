4cf71b028fd6f334f148180f3e2574842823922d
jbureau <julien.bureau02@gmail.com>
2014-12-03 13:55:32 +0100

    [*] BO Diffuseur.java et Annonceur.java

--- src/megacasting/entity/Annonceur.java
+++ src/megacasting/entity/Annonceur.java
@@ -10,8 +10,6 @@ package megacasting.entity;
 public class Annonceur extends Entreprise {
 	
     private long identifiant;
-    private String nom;
-    private String responsable;
     private String siret;
     private Entreprise entreprise;
     
@@ -39,30 +37,6 @@ public class Annonceur extends Entreprise {
 	 */
 	public void setIdentifiant(long identifiant) {
 		this.identifiant = identifiant;
-	}
-	/**
-	 * @return the nom
-	 */
-	public String getNom() {
-		return nom;
-	}
-	/**
-	 * @param nom the nom to set
-	 */
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	/**
-	 * @return the responsable
-	 */
-	public String getResponsable() {
-		return responsable;
-	}
-	/**
-	 * @param responsable the responsable to set
-	 */
-	public void setResponsable(String responsable) {
-		this.responsable = responsable;
 	}
 	/**
 	 * @return the siret
